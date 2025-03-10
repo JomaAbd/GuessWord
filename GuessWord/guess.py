9bf0622f2aae649d22f7c7af286e0efe1238b35e
Joma <jamshidovjoma@gmail.com>
2025-03-10 13:37:41 +0500

    display the letters

--- GuessWord/guess.py
+++ GuessWord/guess.py
@@ -4,3 +4,12 @@ from enwords import words
 def get_word():
     word = random.choice(words)
     return word.upper()
+
+def display(user_letters, word):
+    display_letter = ""
+    for letter in word:
+        if letter in user_letters.upper():
+            display_letter += letter
+        else:
+            display_letter += "_"
+    return display_letter
