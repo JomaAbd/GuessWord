7f2e8e74dac2b4268744845600d56e4934157c7b
Joma <jamshidovjoma@gmail.com>
2025-03-10 15:48:35 +0500

    done

--- GuessWord/guess.py
+++ GuessWord/guess.py
@@ -15,10 +15,11 @@ def display(user_letters, word):
     return display_letter
     
 def play():
-    word = get_word()
-   	word_letters = set(word)
 
+    word = get_word()
+    word_letters = set(word)
     user_letters = ''
+
     print(f"I thought {len(word)} lenght of word. Can you guess?")
     
     while len(word_letters) > 0:
@@ -27,3 +28,14 @@ def play():
             print(f"You entered letters: {user_letters}")
 
         letter = input("Enter a letter: ").upper()
+        if letter in user_letters:
+            print("You have entered this letter. Enter another letter")
+            continue
+        elif letter in word:
+            word_letters.remove(letter)
+            print(f"{letter} - letter correct.")
+        else:
+            print("The letter does not match.")
+        user_letters += letter
+    print(f"Congratulations! The word {word} you guessed in {len(user_letters)} attempts")
+
